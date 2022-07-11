1f7ebd1279aaec030a4ebf3889bc22c67c35637f
kev174 <kev17404@yahoo.co.uk>
2022-07-11 10:38:08 +0100

    updated to 6 from gaming machine

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 # https://www.deadbear.io/simple-serverless-fastapi-with-aws-lambda/
 # https://medium.com/thelorry-product-tech-data/aws-lambda-fastapi-ci-cd-pipeline-with-github-actions-c414866b2d48
 # https://lsh8cin25k.execute-api.us-east-2.amazonaws.com/Dev
-# https://lsh8cin25k.execute-api.us-east-2.amazonaws.com/Dev/kevin/3
+# https://lsh8cin25k.execute-api.us-east-2.amazonaws.com/Dev/kevin/6
 # https://lsh8cin25k.execute-api.us-east-2.amazonaws.com/Dev/api/v1/test/
 # https://lsh8cin25k.execute-api.us-east-2.amazonaws.com/Dev/api/v1/users/
 # https://lsh8cin25k.execute-api.us-east-2.amazonaws.com/Dev/api/v1/posts/
@@ -13,7 +13,7 @@
 # http://127.0.0.1:8000/api/v1/test/      I need the final forward slash here
 # http://127.0.0.1:8000/api/v1/users/
 # http://127.0.0.1:8000/api/v1/posts/
-# http://127.0.0.1:8000/openapi.json      
+# http://127.0.0.1:8000/openapi.json
 
 # uvicorn main:app --reload                 cd into app - cd app
 
@@ -27,7 +27,7 @@ app.include_router(api_router, prefix="/api/v1")
 @app.get("/",  tags=["Endpoint Test"])
 def main_endpoint_test():
     return {"message": "Welcome Kevin to your ROOT CI/CD Pipeline with GitHub Actions!"}
-	
+
 @app.get("/kevin/{item_id}",  tags=["Endpoint Test-2"])
 def main_endpoint_test2(item_id: int):
     return {"Welcome Kevin Cusack to your /kevin endpoint CI/CD Pipeline with GitHub Actions! Id sent=": item_id}
