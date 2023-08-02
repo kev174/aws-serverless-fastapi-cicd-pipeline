9ef87b68e5644d729afbe94630c643afb5cc9722
kev174 <kev17404@yahoo.co.uk>
2023-08-02 13:42:20 +0100

    added comment with link to github actions build pipeline

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@
 # http://127.0.0.1:8000/api/v1/posts/
 # http://127.0.0.1:8000/openapi.json
 
-# uvicorn main:app --reload                 cd into app - cd app
+# cd into app - cd app, uvicorn main:app --reload                 
 # https://www.programcreek.com/python/example/123593/google.cloud.logging.Client
 
 # uvicorn main:app --reload   *** you HAVE to 'cd app' first as the FROM api.api_v1 is not resolved *** 
